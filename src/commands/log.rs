7890ab118c81d616f636f16c519580e9b9a3599d
Samuel Sarle <samuel@asimov.systems>
2025-07-28 15:11:37 +0300

    Add a timestamp diff format helper

--- src/commands/log.rs
+++ src/commands/log.rs
@@ -3,16 +3,18 @@
 use asimov_snapshot::{Snapshotter, storage::Fs};
 use clientele::{StandardOptions, SysexitsError};
 use color_print::{ceprintln, cprintln};
-use jiff::{SpanRound, Timestamp, Unit};
+use jiff::{Zoned, tz::TimeZone};
+
+use crate::timestamps::format_ts_diff;
 
 #[tokio::main]
 pub async fn log(url: &str, _flags: &StandardOptions) -> Result<(), SysexitsError> {
     let ss = Snapshotter::<Fs>::new_fs()
-        .inspect_err(|e| ceprintln!("<s,r>error:</> Failed to create snapshotter: {e}"))?;
+        .inspect_err(|e| ceprintln!("<s,r>error:</> failed to create snapshotter: {e}"))?;
     let snapshots = ss.log(url).await.inspect_err(|e| {
         ceprintln!("<s,r>error:</> failed to fetch snapshot log for `{url}`: {e}")
     })?;
-    let now = Timestamp::now();
+    let now = Zoned::now();
     for ts in snapshots {
         let data = ss.read(url, ts).await.inspect_err(|e| {
             ceprintln!("<s,r>error:</> failed to read snapshot `{ts}` for url `{url}`: {e}")
@@ -21,23 +23,10 @@ pub async fn log(url: &str, _flags: &StandardOptions) -> Result<(), SysexitsErro
         let hash = <sha2::Sha256 as sha2::Digest>::digest(&data);
         let hash = &hex::encode(hash)[..8];
 
-        let diff = now - ts;
+        let diff = format_ts_diff(&now, &ts.to_zoned(TimeZone::UTC))
+            .expect("Unexpectedly failed to format timestamp difference");
 
-        // TODO: largest/smallest rounding units based on the difference
-        // e.g.: if diff >= 1.years() => (1.year(), 1.week())
-        //       if diff >= 1.weeks() => (1.week(), 1.day())
-        //       ...
-
-        let since = diff
-            .round(
-                SpanRound::new()
-                    .days_are_24_hours()
-                    .largest(Unit::Day)
-                    .smallest(Unit::Second),
-            )
-            .unwrap();
-
-        cprintln!("<s>{hash}</> ({since:#} ago)");
+        cprintln!("<s>{hash}</> ({diff:#})");
     }
     Ok(())
 }
