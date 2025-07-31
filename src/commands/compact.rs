c107ba725b0b6e1a05e01ec6579f421e7da483b2
Samuel Sarle <samuel@asimov.systems>
2025-07-31 19:52:59 +0300

    Compact all URLs by default if empty args

--- src/commands/compact.rs
+++ src/commands/compact.rs
@@ -11,8 +11,19 @@ pub async fn compact(urls: &[String], _flags: &StandardOptions) -> Result<(), Sy
     let storage = asimov_snapshot::storage::Fs::for_dir(asimov_root().join("snapshots"))?;
     let ss = Snapshotter::new(Resolver::new(), storage);
 
+    let urls: Vec<String> = if !urls.is_empty() {
+        urls.into()
+    } else {
+        ss.list()
+            .await
+            .inspect_err(|e| ceprintln!("<s,r>error:</> failed to list URLs: {e}"))?
+            .into_iter()
+            .map(|(url, _)| url)
+            .collect()
+    };
+
     for url in urls {
-        ss.compact(url).await.inspect_err(|e| {
+        ss.compact(&url).await.inspect_err(|e| {
             ceprintln!("<s,r>error:</> failed to compact snapshots for `{url}`: {e}")
         })?;
     }
